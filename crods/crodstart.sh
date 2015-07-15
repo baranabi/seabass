51eb0a32007302791705548eaab5789f10c588f8
baran-balkan <baran.balkan@gmail.com>
2015-07-15 12:08:20 -0700

    Modified Dockerfiles to install the binaries through ftp urls instead of copying local binaries, to save space.

--- crods/crodstart.sh
+++ crods/crodstart.sh
@@ -4,7 +4,8 @@ psql -h icat -U postgres -f icatsetup.sql
 
 
 echo INSTALLING IRODS
-rpm -i irods-*.rpm
+rpm -i ftp://ftp.renci.org/pub/irods/releases/4.1.3/centos6/irods-icat-4.1.3-centos6-x86_64.rpm
+rpm -i ftp://ftp.renci.org/pub/irods/releases/4.1.3/centos6/irods-database-plugin-postgres93-1.5-centos6-x86_64.rpm
 echo IRODS INSTALLED
 
 echo SETTING UP IRODS!
