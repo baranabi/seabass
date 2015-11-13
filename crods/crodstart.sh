8fbff089efb9f360cf32634c313197c2c8006f4d
bascibaran <baran.balkan@gmail.com>
2015-11-13 12:01:57 -0700

    first smattering of changes to update seabass to cent7 and irods 4.1.6

--- crods/crodstart.sh
+++ crods/crodstart.sh
@@ -4,8 +4,8 @@ psql -h icat -U postgres -f icatsetup.sql
 
 
 echo INSTALLING IRODS
-rpm -i ftp://ftp.renci.org/pub/irods/releases/4.1.3/centos6/irods-icat-4.1.3-centos6-x86_64.rpm
-rpm -i ftp://ftp.renci.org/pub/irods/releases/4.1.3/centos6/irods-database-plugin-postgres93-1.5-centos6-x86_64.rpm
+rpm -i ftp://ftp.renci.org/pub/irods/releases/4.1.6/centos7/irods-icat-4.1.6-centos7-x86_64.rpm 
+rpm -i ftp://ftp.renci.org/pub/irods/releases/4.1.6/centos7/irods-database-plugin-postgres93-1.6-centos7-x86_64.rpm
 echo IRODS INSTALLED
 
 echo SETTING UP IRODS!
