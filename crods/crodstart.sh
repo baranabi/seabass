8f195b13ec37809649f02c8f1ae38043c0ab8d8e
bascibaran <baran.balkan@gmail.com>
2016-05-20 11:30:02 -0700

    split crodstart.sh into two pieces, 1) build 2) runtime

--- crods/crodstart.sh
+++ crods/crodstart.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-echo INSTALLING IRODS
-rpm -i  ftp://ftp.renci.org/pub/irods/releases/4.1.8/centos7/irods-icat-4.1.8-centos7-x86_64.rpm
-rpm -i  ftp://ftp.renci.org/pub/irods/releases/4.1.8/centos7/irods-database-plugin-postgres-1.8-centos7-x86_64.rpm
-echo IRODS INSTALLED
-
 echo SETTING UP IRODS IES IE
 touch respfile
 /genresp.sh respfile
