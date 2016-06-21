78f06d99420af0a825cd7ba8b1c76cee9e135015
bascibaran <baran.balkan@gmail.com>
2016-06-21 13:12:48 -0700

    documented icat container

--- icat/icatsetup.sql
+++ icat/icatsetup.sql
@@ -1,3 +1,7 @@
+
+-- icatsetup.sql
+-- this script sets up the postgresql ICAT db. 
+-- creates db, default irods role with all privileges.
 CREATE DATABASE "ICAT";
 CREATE USER irods WITH PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE "ICAT" to irods;
